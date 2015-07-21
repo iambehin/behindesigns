099870caa0b263a558d45bcb925ae26c75cd9dfa
iambehin <iambehin@gmail.com>
2015-07-21 14:50:03 -0400

    Creation
    Initial layout for Behin Designs homepage!

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>
+		Behin Designs
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="logo">
+		<h1 id=logoHeader>
+			Behin Designs!
+		</h1>
+	</div>
+	Hi
+
+</body>
+</html>
