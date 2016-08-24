1661d9809984599fa0c571bc8cff5c1ef5724f14
iambehin <iambehin@gmail.com>
2016-08-24 00:17:59 -0400

    v1
    v1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div id="logo">
+	<div class=blockStyle id="logo">
 		<h1 id=logoHeader>
 			Behin Designs!
 		</h1>
@@ -19,8 +19,23 @@
 		If not, please drop me a line.
 		~Behinjamin
 	</pre></article>
-	<nav><footer>
-	Navigation footer: <A href="/behindesigns/">Index</a> |*| <a href="about.php">About</a> |*| <a href="forms.php">Forms</a>
+	<article><pre>
+		Hello, and welcome to home base for Behin Designs!
+		Use the navigation bar below to navigate the site easily.
+		I hope you will find what you're looking for.
+		If not, please drop me a line.
+		~Behinjamin
+	</pre></article>
+	<article><pre>
+		Hello, and welcome to home base for Behin Designs!
+		Use the navigation bar below to navigate the site easily.
+		I hope you will find what you're looking for.
+		If not, please drop me a line.
+		~Behinjamin
+	</pre></article>
+	
+	<nav><footer class=blockStyle>
+		Copyright &#169 2016 Behin
 	</footer></nav>
 </body>
 </html>
